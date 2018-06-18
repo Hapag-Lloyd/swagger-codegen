159458862bbd20060839c1a896d34f0a2f2bcae0
Olaf Neumann <neumaol@hlag.com>
2018-06-18 14:14:06 +0200

    removed warning

--- modules/swagger-codegen-cli/src/main/java/io/swagger/codegen/cmd/ValidateException.java
+++ modules/swagger-codegen-cli/src/main/java/io/swagger/codegen/cmd/ValidateException.java
@@ -4,4 +4,5 @@ package io.swagger.codegen.cmd;
  * Created by takuro on 2017/05/02.
  */
 public class ValidateException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 }
